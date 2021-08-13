dfe82e6967c37294fab316b008ea5f40109b697b
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-08-13 18:33:20 +0500

    updates

--- connect.php
+++ connect.php
@@ -129,31 +129,5 @@ mysqli_close($con);
 
   </div>
 
-  <script type="text/javascript" src="./md5.js"></script>
-        <script type="text/javascript">
-                        function doLogin() {
-                                        <?php if(strlen($chapid) < 1) echo "return true;\n"; ?>
-                                        document.sendin.username.value = document.login.username.value;
-                                        document.sendin.password.value = hexMD5('\011\373\054\364\002\233\266\263\270\373\173\323\234\313\365\337\356');
-                                        document.sendin.submit();
-                                        return false;
-                        }
-        </script>
-        <script type="text/javascript">
-                function formAutoSubmit () {
-                        var frm = document.getElementById("login");
-                        document.getElementById("login").submit();
-                        frm.submit();
-        }
-        window.onload = formAutoSubmit;
-        </script>
-
-        <form id="login" method="post" action="<?php echo $linkloginonly; ?>" onSubmit="return doLogin()">
-        <input name="dst" type="hidden" value="<?php echo $linkorig; ?>" />
-        <input name="popup" type="hidden" value="false" />
-        <input name="username" type="hidden" value="<?php echo $username; ?>"/>
-        <input name="password" type="hidden"/>
-        </form>
-
 </body>
 </html>
