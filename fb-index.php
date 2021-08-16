fb7b9c78a76f9e3afcbdb10ba2ae7e8d47f24f75
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-08-16 14:36:14 +0500

    fb update

--- fb-index.php
+++ fb-index.php
@@ -1,77 +1,53 @@
 <?php
-//initialize facebook sdk
-require 'vendor/autoload.php';
 session_start();
+
+include 'parameters.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
 $fb = new Facebook\Facebook([
- 'app_id' => '352986708826899',
- 'app_secret' => '30e8528feb568199e6e7075451a33562',
- 'default_graph_version' => 'v2.5',
+  'app_id'                => $_SERVER['APP_ID'],
+  'app_secret'            => $_SERVER['APP_SECRET'],
+  'default_graph_version' => $_SERVER['DEFAULT_GRAPH_VERSION'],
 ]);
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email']; // optional
-try {
-if (isset($_SESSION['facebook_access_token'])) {
-$accessToken = $_SESSION['facebook_access_token'];
-} else {
-  $accessToken = $helper->getAccessToken();
-}
-} catch(Facebook\Exceptions\facebookResponseException $e) {
-// When Graph returns an error
-echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-// When validation fails or other local issues
-echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-if (isset($accessToken)) {
-if (isset($_SESSION['facebook_access_token'])) {
-$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-} else {
-// getting short-lived access token
-$_SESSION['facebook_access_token'] = (string) $accessToken;
-  // OAuth 2.0 client handler
-$oAuth2Client = $fb->getOAuth2Client();
-// Exchanges a short-lived access token for a long-lived one
-$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-// setting default access token to be used in script
-$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-}
-// redirect the user to the profile page if it has "code" GET variable
-if (isset($_GET['code'])) {
-header('Location: profile.php');
-}
-// getting basic info about user
-try {
-$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-$requestPicture = $fb->get('/me/picture?redirect=false&height=200'); //getting user picture
-$picture = $requestPicture->getGraphUser();
-$profile = $profile_request->getGraphUser();
-$fbid = $profile->getProperty('id');           // To Get Facebook ID
-$fbfullname = $profile->getProperty('name');   // To Get Facebook full name
-$fbemail = $profile->getProperty('email');    //  To Get Facebook email
-$fbpic = "<img src='".$picture['url']."' class='img-rounded'/>";
-# save the user nformation in session variable
-$_SESSION['fb_id'] = $fbid.'</br>';
-$_SESSION['fb_name'] = $fbfullname.'</br>';
-$_SESSION['fb_email'] = $fbemail.'</br>';
-$_SESSION['fb_pic'] = $fbpic.'</br>';
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-// When Graph returns an error
-echo 'Graph returned an error: ' . $e->getMessage();
-session_destroy();
-// redirecting user back to app login page
-header("Location: ./");
-exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-// When validation fails or other local issues
-echo 'Facebook SDK returned an error: ' . $e->getMessage();
-exit;
-}
-} else {
-// replace your website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and you used            
-$loginUrl = $helper->getLoginUrl('https://phpstack-21306-56790-161818.cloudwaysapps.com', $permissions);
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-}
+
+$helper      = $fb->getRedirectLoginHelper();
+$scope       = array("email");
+$loginUrl    = $helper->getLoginUrl($callBackUrl,$scope);
+
 ?>
+
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Astiisb WiFi</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="bulma.min.css" />
+  <link rel="stylesheet" href="font-awesome\css\font-awesome.min.css" />
+  <script defer src="vendor\fortawesome\font-awesome\js\all.js"></script>
+  <link rel="icon" type="image/png" href="favicomatic\favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="favicomatic\favicon-16x16.png" sizes="16x16" />
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="bg">
+
+    <figure id="logo">
+      <img src="logo.png">
+    </figure>
+
+    <div id="logintext" class="content is-size-6">Or login using:</div>
+    
+    <div id="social">
+      <a href="<?php echo htmlspecialchars($loginUrl);?>" class="facebookBtn smGlobalBtn"></a>
+    </div>
+    <br>
+
+
+  </div>
+
+</body>
+</html>
