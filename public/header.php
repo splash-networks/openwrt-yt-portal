5b22df3ce89eb25f1325330274a593d71a40e0f1
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-06-09 15:56:58 +0500

    redirect url

--- public/header.php
+++ public/header.php
@@ -5,7 +5,6 @@ session_start();
 require 'vendor/autoload.php';
 include '../parameters.php';
 
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
 
